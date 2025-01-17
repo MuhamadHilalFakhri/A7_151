dfea121700d004ef342fd2e0fea1dc9919355abb
hilal555 <muhamadhilal04@gmail.com>
2025-01-17 17:09:54 +0700

    membuat TerapiContainer

---
+++ app/src/main/java/com/example/projekakhir/ui/TerapiContainer.kt
@@ -0,0 +1,31 @@
+package com.example.projekakhir.ui
+
+import com.example.projekakhir.repository.NetworkPasienRepository
+import com.example.projekakhir.repository.PasienRepository
+import com.example.projekakhir.service.PasienService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+
+interface AppContainer {
+    val pasienRepository: PasienRepository
+}
+
+class TerapiContainer: AppContainer {
+    private val baseUrl = "http://10.0.2.2:3000/api/mahasiswa/"
+    private val json = Json{ignoreUnknownKeys = true}
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val pasienService:PasienService by lazy {
+        retrofit.create(PasienService::class.java)
+    }
+
+    override val pasienRepository: PasienRepository by lazy {
+        NetworkPasienRepository(pasienService)
+    }
+
+}
