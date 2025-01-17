444c4f8ed63c47d8aa782b20848113214a0bfe26
hilal555 <muhamadhilal04@gmail.com>
2025-01-17 19:00:52 +0700

    update PenyediaViewModel

--- app/src/main/java/com/example/projekakhir/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/projekakhir/ui/viewmodel/PenyediaViewModel.kt
@@ -1,16 +1,20 @@
 package com.example.projekakhir.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.projekakhir.TerapiApplications
+import com.example.projekakhir.ui.viewmodel.pasien.DetailPasienViewModel
 import com.example.projekakhir.ui.viewmodel.pasien.HomeViewModelPasien
+import com.example.projekakhir.ui.viewmodel.pasien.InsertPasienViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer { HomeViewModelPasien(aplikasiTerapi().container.pasienRepository) }
-
+        initializer { InsertPasienViewModel(aplikasiTerapi().container.pasienRepository) }
+        initializer { DetailPasienViewModel(createSavedStateHandle(),aplikasiTerapi().container.pasienRepository) }
     }
 }
 
