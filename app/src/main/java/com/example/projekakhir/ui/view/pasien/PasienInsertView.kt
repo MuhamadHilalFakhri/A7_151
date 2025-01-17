e694626ea58f9c478135b9ab95295b600315436e
hilal555 <muhamadhilal04@gmail.com>
2025-01-17 17:52:17 +0700

    membuat fungsi EntryBodyPasien

--- app/src/main/java/com/example/projekakhir/ui/view/pasien/PasienInsertView.kt
+++ app/src/main/java/com/example/projekakhir/ui/view/pasien/PasienInsertView.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.projekakhir.navigation.DestinasiNavigasi
 import com.example.projekakhir.ui.viewmodel.pasien.InsertPasienUiEvent
+import com.example.projekakhir.ui.viewmodel.pasien.InsertPasienUiState
 
 object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
@@ -21,6 +24,33 @@ object DestinasiEntry: DestinasiNavigasi {
 
 
 
+
+@Composable
+fun EntryBodyPasien(
+    insertUiState: InsertPasienUiState,
+    onPasienValueChange: (InsertPasienUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputPasien(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onPasienValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
