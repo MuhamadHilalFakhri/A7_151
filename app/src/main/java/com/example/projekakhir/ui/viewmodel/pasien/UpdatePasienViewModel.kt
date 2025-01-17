39f173c717315e025a326782c0d17fefbc97c149
hilal555 <muhamadhilal04@gmail.com>
2025-01-17 20:55:23 +0700

    membuat UpdatePasienViewModel

---
+++ app/src/main/java/com/example/projekakhir/ui/viewmodel/pasien/UpdatePasienViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.projekakhir.ui.viewmodel.pasien
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projekakhir.model.Pasien
+import com.example.projekakhir.repository.PasienRepository
+import com.example.projekakhir.ui.view.pasien.DestinasiUpdate
+import kotlinx.coroutines.launch
+
+
+class UpdatePasienViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val pasienRepository: PasienRepository
+) : ViewModel() {
+    val idPasien: Int = checkNotNull(savedStateHandle[DestinasiUpdate.idpasien])
+
+    var uiState by mutableStateOf(InsertPasienUiState())
+        private set
+
+    init {
+        getPasien()
+    }
+
+    // Fungsi untuk mengambil data pasien
+    private fun getPasien() {
+        viewModelScope.launch {
+            try {
+                val pasien = pasienRepository.getPasienById(idPasien)
+                pasien?.let {
+                    uiState = it.toUiStatePasien() // Update UI State dengan data pasien
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    // Fungsi untuk melakukan update pasien
+    fun updatePasien(idPasien: Int, pasien: Pasien) {
+        viewModelScope.launch {
+            try {
+                pasienRepository.updatePasien(idPasien, pasien) // Panggil repository untuk update
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    // Fungsi untuk memperbarui UI State
+    fun updatePasienState(insertUiEvent: InsertPasienUiEvent) {
+        uiState = uiState.copy(insertUiEvent = insertUiEvent)
+    }
+}
