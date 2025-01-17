e58852563ebcf908fa77ccd958c0b11afd9f2b24
hilal555 <muhamadhilal04@gmail.com>
2025-01-17 16:56:30 +0700

    membuat fungsi ketika error

--- app/src/main/java/com/example/projekakhir/ui/view/pasien/PasienHomeView.kt
+++ app/src/main/java/com/example/projekakhir/ui/view/pasien/PasienHomeView.kt
@@ -1,5 +1,6 @@
 package com.example.projekakhir.ui.view.pasien
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -21,9 +23,31 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.projekakhir.R
 import com.example.projekakhir.model.Pasien
+import com.example.projekakhir.ui.viewmodel.pasien.HomeUiState
 
+
+@Composable
+fun OnError(retryAction:()->Unit, modifier: Modifier = Modifier){
+    Column(
+        modifier=modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
 
 
 @Composable
