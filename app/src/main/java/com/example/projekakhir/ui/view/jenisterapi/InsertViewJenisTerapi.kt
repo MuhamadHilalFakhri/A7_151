5f1d718eff4150ea682d46df185a6daa6b061438
hilal555 <muhamadhilal04@gmail.com>
2025-01-22 23:12:44 +0700

    membuat fungsi EntryJenisTerapiScreen

--- app/src/main/java/com/example/projekakhir/ui/view/jenisterapi/InsertViewJenisTerapi.kt
+++ app/src/main/java/com/example/projekakhir/ui/view/jenisterapi/InsertViewJenisTerapi.kt
@@ -4,25 +4,72 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.projekakhir.navigation.DestinasiNavigasi
+import com.example.projekakhir.ui.custom.CostumeTopAppBar
+import com.example.projekakhir.ui.viewmodel.PenyediaViewModel
 import com.example.projekakhir.ui.viewmodel.sesiterapi.InsertJenisTerapiUiEvent
 import com.example.projekakhir.ui.viewmodel.sesiterapi.InsertJenisTerapiUiState
+import com.example.projekakhir.ui.viewmodel.sesiterapi.InsertJenisTerapiViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntryJenisTerapi : DestinasiNavigasi {
     override val route = "item_entry_jenis_terapi"
     override val titleRes = "Entry Jenis Terapi"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryJenisTerapiScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertJenisTerapiViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntryJenisTerapi.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBodyJenisTerapi(
+            insertUiState = viewModel.uiState,
+            onJenisTerapiValueChange = viewModel::updateInsertJenisTerapiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertJenisTerapi()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun EntryBodyJenisTerapi(
