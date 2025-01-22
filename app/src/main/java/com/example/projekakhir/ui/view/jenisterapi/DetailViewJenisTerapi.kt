2176e0fb42091a3ad0bb4939994c52f93b1b6dec
hilal555 <muhamadhilal04@gmail.com>
2025-01-22 23:02:17 +0700

    membuat fungsi ItemDetailJenisTerapi

--- app/src/main/java/com/example/projekakhir/ui/view/jenisterapi/DetailViewJenisTerapi.kt
+++ app/src/main/java/com/example/projekakhir/ui/view/jenisterapi/DetailViewJenisTerapi.kt
@@ -1,14 +1,21 @@
 package com.example.projekakhir.ui.view.jenisterapi
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.projekakhir.model.JenisTerapi
 import com.example.projekakhir.navigation.DestinasiNavigasi
 
 object DestinasiDetailJenisTerapi : DestinasiNavigasi {
@@ -19,6 +26,27 @@ object DestinasiDetailJenisTerapi : DestinasiNavigasi {
 }
 
 
+
+@Composable
+fun ItemDetailJenisTerapi(
+    jenisTerapi: JenisTerapi
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            ComponentDetailJenisTerapi(judul = "ID Jenis Terapi", isinya = jenisTerapi.id_jenis_terapi.toString())
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJenisTerapi(judul = "Nama Jenis Terapi", isinya = jenisTerapi.nama_jenis_terapi)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJenisTerapi(judul = "Deskripsi Terapi", isinya = jenisTerapi.deskripsi_terapi)
+        }
+    }
+}
 
 @Composable
 fun ComponentDetailJenisTerapi(
