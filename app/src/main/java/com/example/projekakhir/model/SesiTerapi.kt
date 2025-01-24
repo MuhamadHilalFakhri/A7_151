b1ab5c833e0f5f8900972ce3e6e7a53d5a056c35
hilal555 <muhamadhilal04@gmail.com>
2025-01-24 18:13:27 +0700

    membuat model sesi terapi

---
+++ app/src/main/java/com/example/projekakhir/model/SesiTerapi.kt
@@ -0,0 +1,27 @@
+package com.example.projekakhir.model
+
+import kotlinx.serialization.*
+
+@Serializable
+data class SesiTerapi(
+    val id_sesi: Int,
+    val id_pasien: Int,
+    val id_terapis: Int,
+    val id_jenis_terapi: Int,
+    val tanggal_sesi: String,
+    val catatan_sesi: String? = null //Catatan sesi opsional
+)
+
+@Serializable
+data class AllSesiResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<SesiTerapi>
+)
+
+@Serializable
+data class SesiDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: SesiTerapi
+)
