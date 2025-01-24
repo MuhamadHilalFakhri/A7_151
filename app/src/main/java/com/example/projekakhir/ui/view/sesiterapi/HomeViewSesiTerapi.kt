4b071c652f70b0180fd341378472a00640088494
hilal555 <muhamadhilal04@gmail.com>
2025-01-24 18:21:45 +0700

    membuat fungsi HomeSesiTerapi

--- app/src/main/java/com/example/projekakhir/ui/view/sesiterapi/HomeViewSesiTerapi.kt
+++ app/src/main/java/com/example/projekakhir/ui/view/sesiterapi/HomeViewSesiTerapi.kt
@@ -15,33 +15,88 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.projekakhir.R
 import com.example.projekakhir.model.SesiTerapi
 import com.example.projekakhir.navigation.DestinasiNavigasi
+import com.example.projekakhir.ui.custom.CostumeTopAppBar
+import com.example.projekakhir.ui.viewmodel.PenyediaViewModel
 import com.example.projekakhir.ui.viewmodel.sesiterapi.HomeUiState
+import com.example.projekakhir.ui.viewmodel.sesiterapi.HomeViewModelSesiTerapi
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.TimeZone
 
-object DestinasiHomeSesi : DestinasiNavigasi {
+object DestinasiHomeSesiTerapi : DestinasiNavigasi {
     override val route = "home sesi terapi"
     override val titleRes = "Home Sesi Terapi"
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeSesiTerapi(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+    viewModel: HomeViewModelSesiTerapi = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHomeSesiTerapi.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getSesiTerapi()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Sesi Terapi")
+            }
+        },
+    ) { innerPadding ->
+        HomeStatusSesiTerapi(
+            homeUiState = viewModel.sesiTerapiUIState,
+            retryAction = { viewModel.getSesiTerapi() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deleteSesiTerapi(it.id_sesi)
+                viewModel.getSesiTerapi()
+            }
+        )
+    }
+}
+
 
 @Composable
 fun HomeStatusSesiTerapi(
